c8e4bba73c0f1d7a94f34526f5a12a827567f8ba
Ador Benko <abenko@brainsum.com>
2021-08-09 16:50:51 +0300

    DAMO-210 | Setting up and integrating react widget into drupal

---
+++ assets_wfa_widget/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+
+gulp.task(
+  "default",
+  gulp.parallel(
+    () => {
+      return gulp.src("./build/static/css/main.*.chunk.css").pipe(rename("./buildtoembed/main.assets_wfa_widget.chunk.css")).pipe(gulp.dest("."));
+    },
+    () => {
+      return gulp.src("./build/static/js/runtime-main.*.js").pipe(rename("./buildtoembed/runtime-main.assets_wfa_widget.js")).pipe(gulp.dest("."));
+    },
+    () => {
+      return gulp.src("./build/static/js/2.*.chunk.js").pipe(rename("./buildtoembed/2.assets_wfa_widget.chunk.js")).pipe(gulp.dest("."));
+    },
+    () => {
+      return gulp.src("./build/static/js/main.*.chunk.js").pipe(rename("./buildtoembed/main.assets_wfa_widget.chunk.js")).pipe(gulp.dest("."));
+    }
+  )
+);
