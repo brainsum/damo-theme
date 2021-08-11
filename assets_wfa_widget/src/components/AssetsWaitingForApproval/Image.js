868e56417620f3a744cd09785d79fffb084b6c8d
Ador Benko <abenko@brainsum.com>
2021-08-11 15:43:56 +0300

    DAMO-214 | Bug fixes

--- assets_wfa_widget/src/components/AssetsWaitingForApproval/Image.js
+++ assets_wfa_widget/src/components/AssetsWaitingForApproval/Image.js
@@ -1,27 +1,28 @@
 import styles from "./AssetsWaitingForApproval.module.scss";
-import icon from "./icon.svg";
+import checkmarkIcon from "./checkmark_icon.svg";
 
 const Image = ({ image, clickHandler }) => {
   const detailsClickHandler = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log("CLICKED");
+    console.log("Details button clicked");
   };
 
   return (
     <div className={`views-row`}>
-      <div className={`card card-inverse card--media ${styles["image-container"]}`}>
+      <div className={`card card-inverse card--media ${styles["image__container"]}`}>
         <img
           src={image.src}
           width="500"
           height="334"
           loading="lazy"
-          className={`card-img img-fluid ${image.selected ? styles.selected : ""}`}
+          className={`card-img img-fluid ${image.selected ? styles["image--selected"] : ""}`}
           style={{ maxWidth: "234.812px", maxHeight: "158.734px" }}
-          alt="myImage"></img>
-        <div className={styles["image-hover"]} onClick={() => clickHandler(image)}>
-          <img src={icon} alt="" className={styles.icon} />
-          <button className={styles["detail-button"]} onClick={detailsClickHandler}>
+          alt={image.alt}></img>
+
+        <div className={styles["image__hover-layer"]} onClick={() => clickHandler(image)}>
+          <img src={checkmarkIcon} alt="checkmark icon" className={styles["checkmark-icon"]} />
+          <button className={styles["details-button"]} onClick={detailsClickHandler}>
             details
           </button>
         </div>
