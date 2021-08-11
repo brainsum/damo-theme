0be98b312cc0385caa6a21ecea977daa431c4f5d
Ador Benko <abenko@brainsum.com>
2021-08-11 14:04:22 +0300

    DAMO-214 | Approve process: level 1 - without api calls

---
+++ assets_wfa_widget/src/components/AssetsWaitingForApproval/api.js
@@ -0,0 +1,19 @@
+const api = [
+  {
+    id: 0,
+    src: "http://damopen.docker.localhost:8000/system/files/styles/gallery_item/private/media/image/image/2021-08/_DSC9252_comp_3_3_other_hi_res_no_badge.jpg?h=1150e136&itok=ebG6hEhq",
+  },
+  {
+    id: 1,
+    src: "http://damopen.docker.localhost:8000/system/files/styles/gallery_item/private/media/image/image/2021-08/_DSC9384_other_hi_res_no_badge.jpg?h=41cd9748&itok=ucLbZois",
+  },
+  {
+    id: 2,
+    src: "http://damopen.docker.localhost:8000/system/files/styles/gallery_item/private/media/image/image/2021-08/_DSC9252_comp_3_3_other_hi_res_no_badge.jpg?h=1150e136&itok=ebG6hEhq",
+  },
+  {
+    id: 3,
+    src: "http://damopen.docker.localhost:8000/system/files/styles/gallery_item/private/media/image/image/2021-08/_DSC9384_other_hi_res_no_badge.jpg?h=41cd9748&itok=ucLbZois",
+  },
+];
+export default api;
