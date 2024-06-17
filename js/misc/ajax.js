3203c50fd796bfae5b2f25b7be31891eb7221b23
Adam Herczeg <aherczeg@brainsum.com>
2024-06-17 08:41:00 +0200

    DAMO-308 | Popup needs 2 clicks.

--- js/misc/ajax.js
+++ js/misc/ajax.js
@@ -116,76 +116,76 @@
    * @param {Array.<Drupal.AjaxCommands~commandDefinition>} response
    * @param {number} status
    */
-  Drupal.Ajax.prototype.success = function (response, status) {
-    if (this.progress.element) {
+  // Drupal.Ajax.prototype.success = function (response, status) {
+  //   if (this.progress.element) {
 
-      // Stop a glyphicon throbber.
-      if (this.progress.glyphicon) {
-        this.glyphiconStop(this.progress.element);
-      }
-      // Remove the progress element.
-      else {
-        this.progress.element.remove();
-      }
+  //     // Stop a glyphicon throbber.
+  //     if (this.progress.glyphicon) {
+  //       this.glyphiconStop(this.progress.element);
+  //     }
+  //     // Remove the progress element.
+  //     else {
+  //       this.progress.element.remove();
+  //     }
 
-      // Remove any message set.
-      this.progress.element.parent().find('.message').remove();
-    }
+  //     // Remove any message set.
+  //     this.progress.element.parent().find('.message').remove();
+  //   }
 
-    // --------------------------------------------------------
-    // Everything below is from core/misc/ajax.js.
-    // --------------------------------------------------------
+  //   // --------------------------------------------------------
+  //   // Everything below is from core/misc/ajax.js.
+  //   // --------------------------------------------------------
 
-    if (this.progress.object) {
-      this.progress.object.stopMonitoring();
-    }
-    $(this.element).prop('disabled', false);
+  //   if (this.progress.object) {
+  //     this.progress.object.stopMonitoring();
+  //   }
+  //   $(this.element).prop('disabled', false);
 
-    // Save element's ancestors tree so if the element is removed from the dom
-    // we can try to refocus one of its parents. Using addBack reverse the
-    // result array, meaning that index 0 is the highest parent in the hierarchy
-    // in this situation it is usually a <form> element.
-    var elementParents = $(this.element).parents('[data-drupal-selector]').addBack().toArray();
+  //   // Save element's ancestors tree so if the element is removed from the dom
+  //   // we can try to refocus one of its parents. Using addBack reverse the
+  //   // result array, meaning that index 0 is the highest parent in the hierarchy
+  //   // in this situation it is usually a <form> element.
+  //   var elementParents = $(this.element).parents('[data-drupal-selector]').addBack().toArray();
 
-    // Track if any command is altering the focus so we can avoid changing the
-    // focus set by the Ajax command.
-    var focusChanged = false;
-    for (var i in response) {
-      if (response.hasOwnProperty(i) && response[i].command && this.commands[response[i].command]) {
-        this.commands[response[i].command](this, response[i], status);
-        if (response[i].command === 'invoke' && response[i].method === 'focus') {
-          focusChanged = true;
-        }
-      }
-    }
+  //   // Track if any command is altering the focus so we can avoid changing the
+  //   // focus set by the Ajax command.
+  //   var focusChanged = false;
+  //   for (var i in response) {
+  //     if (response.hasOwnProperty(i) && response[i].command && this.commands[response[i].command]) {
+  //       this.commands[response[i].command](this, response[i], status);
+  //       if (response[i].command === 'invoke' && response[i].method === 'focus') {
+  //         focusChanged = true;
+  //       }
+  //     }
+  //   }
 
-    // If the focus hasn't be changed by the ajax commands, try to refocus the
-    // triggering element or one of its parents if that element does not exist
-    // anymore.
-    if (!focusChanged && this.element && !$(this.element).data('disable-refocus')) {
-      var target = false;
+  //   // If the focus hasn't be changed by the ajax commands, try to refocus the
+  //   // triggering element or one of its parents if that element does not exist
+  //   // anymore.
+  //   if (!focusChanged && this.element && !$(this.element).data('disable-refocus')) {
+  //     var target = false;
 
-      for (var n = elementParents.length - 1; !target && n > 0; n--) {
-        target = document.querySelector('[data-drupal-selector="' + elementParents[n].getAttribute('data-drupal-selector') + '"]');
-      }
+  //     for (var n = elementParents.length - 1; !target && n > 0; n--) {
+  //       target = document.querySelector('[data-drupal-selector="' + elementParents[n].getAttribute('data-drupal-selector') + '"]');
+  //     }
 
-      if (target) {
-        $(target).trigger('focus');
-      }
-    }
+  //     if (target) {
+  //       $(target).trigger('focus');
+  //     }
+  //   }
 
-    // Reattach behaviors, if they were detached in beforeSerialize(). The
-    // attachBehaviors() called on the new content from processing the response
-    // commands is not sufficient, because behaviors from the entire form need
-    // to be reattached.
-    if (this.$form) {
-      var settings = this.settings || drupalSettings;
-      Drupal.attachBehaviors(this.$form.get(0), settings);
-    }
+  //   // Reattach behaviors, if they were detached in beforeSerialize(). The
+  //   // attachBehaviors() called on the new content from processing the response
+  //   // commands is not sufficient, because behaviors from the entire form need
+  //   // to be reattached.
+  //   if (this.$form) {
+  //     var settings = this.settings || drupalSettings;
+  //     Drupal.attachBehaviors(this.$form.get(0), settings);
+  //   }
 
-    // Remove any response-specific settings so they don't get used on the next
-    // call by mistake.
-    this.settings = null;
-  };
+  //   // Remove any response-specific settings so they don't get used on the next
+  //   // call by mistake.
+  //   this.settings = null;
+  // };
 
 })(jQuery, this, Drupal, drupalSettings);
